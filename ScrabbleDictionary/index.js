a1ed15d11da43ffa9732b9d9b81cdede3021d8d7
James Campbell <james@supmenow.com>
2016-08-25 09:01:01 +0100

    Simple scrabble bot.

---
+++ ScrabbleDictionary/index.js
@@ -0,0 +1,33 @@
+var Pozi = require('pozi')
+var bot = new Pozi.Bot({
+   json_file_store: './data'
+});
+
+// Find CSV Dictionary
+var dictionary = [
+  "sat",
+  "hat",
+  "mat"
+]
+
+bot.on(['message_received'], function(bot, utterance) {
+
+  var letters = utterance.text.split("").filter(function(letter){
+      return letter != " "
+  })
+
+  var words = dictionary.filter(function(word) {
+
+    var detectedLetters = letters.filter(function(letter){
+      return word.indexOf(letter) >= 0
+    })
+
+    return detectedLetters.length >= word.length
+  })
+
+  bot.reply("Here are the words you can make with the letters: " + letters.join(" "))
+  bot.reply(words.join(", "))
+});
+
+// Wire up to twillio
+bot.spawn()
