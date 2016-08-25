54b9b4de838745f168ee442e45394c8a82029eb5
James Campbell <james@supmenow.com>
2016-08-25 08:53:11 +0100

    Outlining the first 3 examples.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Fix port conflixt via Express.js proxying
 //require('./EchoBot/index.js') // Start EchoBot Example
-require('./Timetable/index.js') // Start Timetable Example
+require('./TrainTimetable/index.js') // Start Train Timetable Example
+//require('./ScrabbleDictionary/index.js') // Start Scrabble Dictionary Example
