b10f35d925fd1af39674ef920212058732d603a4
James Campbell <james@supmenow.com>
2016-08-26 21:02:03 +0100

    Ported timetable example.

--- TrainTimetable/index.js
+++ TrainTimetable/index.js
@@ -2,56 +2,44 @@ var Pozi = require('pozi')
 var bot = new Pozi.Bot();
 
 // - Use Twitter
+var timetable = [
+  {
+    time: "7:00",
+    destination: "London"
+  },
+  {
+    time: "8:00",
+    destination: "Swindon"
+  },
+  {
+    time: "9:00",
+    destination: "London"
+  },
+  {
+    time: "10:00",
+    destination: "Swindon"
+  },
+  {
+    time: "11:00",
+    destination: "London"
+  },
+  {
+    time: "12:00",
+    destination: "Swindon"
+  }
+]
 
-//
-// // TODO: Replace with real data
-// var timetable = [
-//   {
-//     time: "7:00",
-//     destination: "London"
-//   },
-//   {
-//     time: "8:00",
-//     destination: "Swindon"
-//   },
-//   {
-//     time: "9:00",
-//     destination: "London"
-//   },
-//   {
-//     time: "10:00",
-//     destination: "Swindon"
-//   },
-//   {
-//     time: "11:00",
-//     destination: "London"
-//   },
-//   {
-//     time: "12:00",
-//     destination: "Swindon"
-//   }
-// ]
-//
-// bot.hears([/timetable/], ['message_received'], function(bot, utterance, context) {
-//
-//   var rows = timetable.map(function(item){
-//     return item.time + " " + item.destination
-//   })
-//
-//   //List timetable
-//   bot.reply("The timetable for today is:")
-//   bot.reply(rows.join("\n"));
-// });
-//
-// // Put into array
-// bot.hears([/thanks/, /cheers/, /thank you/, /ta/, /thx/, /ty/], ['message_received'], function(bot, utterance, context) {
-//   bot.reply("No problem!")
-// });
-//
-// // Hook up to twitter
-// bot.spawn({
-//   client: 'twitter',
-//   name: 'TrainTimesBot'
-// })
+bot.hears(/timetable/, function(message, session) {
+
+  var rows = timetable.map(function(item){
+    return item.time + " " + item.destination
+  })
+
+  message.send("The timetable for today is: \n" + rows.join("\n"))
+});
+
+bot.hears([/thanks/, /cheers/, /thank you/, /ta/, /thx/, /ty/], function(message, session) {
+  session.send("No problem!")
+});
 
 module.exports = Pozi.botMiddleware(bot)
