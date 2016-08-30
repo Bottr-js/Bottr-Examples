36b320cbd61d46bf09015a5e58fa3b30104e938f
James Campbell <james@supmenow.com>
2016-08-30 14:35:39 +0100

    Implement statistics.

--- EchoBot/index.js
+++ EchoBot/index.js
@@ -3,8 +3,44 @@ var fs = require('fs');
 var bot = new Pozi.Bot();
 
 bot.use(new Pozi.FacebookMessengerClient())
+bot.use(new Pozi.MemoryStorage())
 
-bot.on('message_received', function(message, session) {
+bot.on('message_received', function(message, session, next) {
+
+ // Get existing context or use defaults if values don't exist
+ var context = session.getUserContext({
+   messageCount: 0,
+   wordCount: 0,
+ })
+
+ // Calculate new statistics
+ context.messageCount ++
+
+ var words = message.text.split(" ")
+ context.wordCount += words.length
+
+ //Store new values into context
+ session.updateUserContext(context)
+
+ //Tell bot to let other handlers process the message
+ next()
+});
+
+bot.hears(/\/stats/, function(message, session) {
+
+ var context = session.getUserContext()
+
+ // Send the total number of messages to the
+ // user
+ session.send("Total Message Count: " + context.messageCount)
+
+ // Send the total number of words to the
+ // user
+ session.send("Total Word Count: " + context.wordCount)
+});
+
+bot.hears([/.+/], function(message, session) {
+  // Repeat what the user sent us
   session.send(message.text)
 });
 
